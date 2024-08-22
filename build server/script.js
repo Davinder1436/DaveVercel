edec248f7a1dd9115521aac58fdf57b9a65544fd
Davinder1436 <davindersingh1436@gmail.com>
2024-08-22 17:40:28 +0530

    s3 setup

--- build server/script.js
+++ build server/script.js
@@ -1,6 +1,20 @@
 const {exec} = require('child_process');
 const path = require('path');
 const fs = require('fs');
+const {S3Client, PutObjectCommand} = require("@aws-sdk/client-s3");
+const dotenv = require('dotenv');
+dotenv.config()
+const mime = require('mime-types');
+
+const s3 = new S3Client({credentials:{
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    
+},
+region: process.env.AWS_REGION});
+
+const Project_Id = process.env.PROJECT_ID
+
 async function init(){
 
     const dirPath = path.join(__dirname, 'repo');
@@ -13,7 +27,7 @@ async function init(){
     p.stdout.on('error',(data)=>{
         console.log(data.tostring())
     })
-    p.stdout.on('close',(data)=>{
+    p.stdout.on('close',async (data)=>{
         console.log("Build Complete")
         const distPath = path.join(__dirname, 'repo','dist')
 
@@ -22,7 +36,21 @@ async function init(){
         for(const filepath in distFolderContent){
             if( fs.lstatSync(filepath).isDirectory){
                 continue;
-            }}
+            }
+        
+            const command = PutObjectCommand({
+                Bucket: process.env.AWS_BUCKET_NAME,
+                Key:`DaveVercel/${Project_Id}/${filepath}`,
+                Body: fs.createReadStream(filepath),
+                ContentType: mime.lookup(filepath)
+            })
+
+            await s3.send(command);
+            console.log('uploaded ' + filepath)
+        
+        }
+
+            
             
 
     })
